ea6f87735d58c6c4e27a1e5ab748e19fab48554f
blue <blue@odin.unm.edu>
2020-11-20 16:48:03 -0700

    added second traceroute destination

--- sync/basicTest.py
+++ sync/basicTest.py
@@ -7,7 +7,7 @@ from scapy.all import *
 
 logging.getLogger("speedtestLog")
 now = datetime.now()
-file_date = now.strftime("%Y-%m-%d@%H:%m")
+file_date = now.strftime("%Y-%m-%dT%H:%m")
 
 logging.basicConfig(filename="speedtestAuto-" + file_date + ".log", level=logging.DEBUG)
 
@@ -29,8 +29,8 @@ logging.info("Speedtest completed. Pic URL: " + srURL)
 logging.info("Download Speed: " + str(srDownloadSpeed) + "mbps")
 logging.info("Upload Speed: " + str(srUploadSpeed) + "mbps")
 
-tr_answer, tr_unanswered = traceroute(testDirectives.HOST_MTR_1)
-graphFileName = "traceroute-"+file_date+".svg"
+tr_answer, tr_unanswered = traceroute(testDirectives.HOST_MTR_1, testDirects.HOST_MTR_2)
+graphFileName = "tracerouteGraph-" + file_date + ".svg"
 graph = tr_answer.graph(target="/app/"+graphFileName)
 
 logging.info("Saved traceroute to " + graphFileName)
